5c4b41f4f72951e3eb97a8157d22751282fcf7fb
Kaur <kaur.palang@hotmail.com>
2019-02-02 14:31:17 +0200

    Tiny bit more descriptive swagger page

--- src/main/java/ituk/ttu/ee/hubbackend/config/SwaggerConfig.java
+++ src/main/java/ituk/ttu/ee/hubbackend/config/SwaggerConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -17,9 +17,20 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket endpoint() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage("ituk.ttu.ee.hubbackend")).build();
-
+                .select().apis(RequestHandlerSelectors.basePackage("ituk.ttu.ee.hubbackend")).build().apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("ITÜK hub backend")
+                .description("ITÜK hub backend endpoint listing")
+                .version("0.0.1-SNAPSHOT")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                //.contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .build();
+    }
+
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
