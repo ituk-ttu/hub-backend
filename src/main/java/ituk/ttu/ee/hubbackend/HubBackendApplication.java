80961eb505ff5afedb90962b876ae3c700cc0f1c
Kroot Grete Mand <krootgrete.mand@nortal.com>
2019-02-02 12:04:58 +0200

    initial project

---
+++ src/main/java/ituk/ttu/ee/hubbackend/HubBackendApplication.java
@@ -0,0 +1,14 @@
+package ituk.ttu.ee.hubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubBackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ituk/ttu/ee/hubbackend/HubBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package ituk.ttu.ee.hubbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HubBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
